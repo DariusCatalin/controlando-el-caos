31d05c07b111d0fe099e7efa2e37b319d2727097
adonis25042 <adonis25042@alumnes.ilerna.com>
2025-10-31 12:43:05 +0100

    Commit rama-porcentaje
    He añadido que ese pueden hacer porcentajes a la calculadora

--- src/Calculadora_/calculadora.java
+++ src/Calculadora_/calculadora.java
@@ -2,24 +2,26 @@ package Calculadora_;
 
 import java.util.Scanner;
 
-public class calculadora {
-	
-	    public static void main(String[] args) {
-			Scanner sc = new Scanner(System.in);
+// Es buena práctica que el nombre de la clase empiece con mayúscula
+public class calculadora { 
+    
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
 
-	        System.out.println("=== Calculadora Básica ===");
-	        int a;
-	        int b;
+            System.out.println("=== Calculadora Básica ===");
+            int a;
+            int b;
 
-			System.out.print("a = ");
-			a = sc.nextInt();
-			System.out.print("b = ");
-			b = sc.nextInt();
-	 
-	        System.out.println("Suma: " + (a + b));
-	        System.out.println("Resta: " + (a - b));
-	        System.out.println("Multiplicación: " + (a * b));
-	        System.out.println("División: " + (a / b));
-	    }
-	}
-
+            System.out.print("a = ");
+            a = sc.nextInt();
+            System.out.print("b = ");
+            b = sc.nextInt();
+      
+            System.out.println("Suma: " + (a + b));
+            System.out.println("Resta: " + (a - b));
+            System.out.println("Multiplicación: " + (a * b));
+            System.out.println("División: " + (a / b));
+            
+            System.out.println("Porcentaje (b% de a): " + (a * b / 100.0));
+        }
+    }
